94103e3e1fd931e40281f1ea1bb081dd3b845366
StrawberryLuna <103065045+StrawberryLuna@users.noreply.github.com>
2023-11-10 21:31:07 -0600

    Updated timer

--- Assets/SpawnCoinScript.cs
+++ Assets/SpawnCoinScript.cs
@@ -2,23 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnCointScript : MonoBehaviour
+public class SpawnCoinScript : MonoBehaviour
 {
-    public GameObject coinPrefab;
+    public GameObject theCoin;
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Rotate the game object around its local Y-axis
+        gameObject.transform.Rotate(0f, 1f, 0f, Space.Self);
     }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            Vector3 randomPosition = new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10));
-            Instantiate(coinPrefab, randomPosition, Quaternion.identity);
-            // Optionally, you can destroy the coin after spawning to prevent multiple spawns in rapid succession.
+            // Destroy the current coin
             Destroy(gameObject);
+
+            // Instantiate a new coin at a random position
+            Vector3 randomPosition = new Vector3(Random.Range(-5, 5), 1, Random.Range(-5, 5));
+            Instantiate(theCoin, randomPosition, Quaternion.identity);
         }
     }
 }
