3f8eb5d0421552544c47e1cf3b2a1fbce1e527b9
StrawberryLuna <103065045+StrawberryLuna@users.noreply.github.com>
2023-11-27 23:35:06 -0600

    add point

--- Assets/SpawnCoinScript.cs
+++ Assets/SpawnCoinScript.cs
@@ -1,34 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class SpawnCoinScript : MonoBehaviour
 {
     public GameObject theCoin;
-    public GameObject Player;
     public AudioSource collectionSoundEffect;
-
-    // Update is called once per frame
+    public TextMeshProUGUI coinText;
+    private int collectedCoins = 0;
     void Update()
     {
-        // Rotate the game object around its local Y-axis
+        // Makes the coin spin
         gameObject.transform.Rotate(0f, 1f, 0f, Space.Self);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Player")
+        //if (other.gameObject.tag == "Player") //original
+        if (other.gameObject.CompareTag("Player"))
         {
-            Debug.Log("In collision");
-            // Destroy the current coin
-            
-            Destroy(gameObject);
+            collectedCoins++;
+            coinText.text = "Points: " + collectedCoins.ToString();
+            Debug.Log("Collected coins: " + collectedCoins);
+
             collectionSoundEffect.Play();
+            
+            // Destroys coin
+            Destroy(gameObject);
 
             // Instantiate a new coin at a random position
             Vector3 randomPosition = new Vector3(Random.Range(-5, 5), 1, Random.Range(-5, 5));
             Instantiate(theCoin, randomPosition, Quaternion.identity);
+            
         }
     }
 }
