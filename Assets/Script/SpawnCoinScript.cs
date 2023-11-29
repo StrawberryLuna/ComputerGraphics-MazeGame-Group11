14a8d0ce94c1255ecfa6c1fb4c3db2bb6291ef2d
StrawberryLuna <103065045+StrawberryLuna@users.noreply.github.com>
2023-11-28 20:56:15 -0600

    updated point
    when you quit it resets scores/points

--- Assets/Script/SpawnCoinScript.cs
+++ Assets/Script/SpawnCoinScript.cs
@@ -1,77 +1,3 @@
-/*using System.Collections;
-using TMPro;
-using UnityEngine;
-
-public class SpawnCoinScript : MonoBehaviour
-{
-    public TextMeshProUGUI CoinText;
-    public TextMeshProUGUI HighScoreText;
-    private int coin = 0;
-    private int highScore = 0;
-    private const string HighScoreKey = "HighScore";
-
-
-    private void Start()
-    {
-        // Load the high score from PlayerPrefs
-        highScore = PlayerPrefs.GetInt(HighScoreKey, 0);
-        UpdateHighScoreText();
-    }
-
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.transform.CompareTag("CoinTag"))
-        {
-            coin++; //increment point
-            CoinText.text = "Points: " + coin.ToString();
-            Debug.Log("Collected coins: " + coin);
-            //For highscore based on coin/points
-             if (coin > highScore)
-            {
-                highScore = coin;
-                PlayerPrefs.SetInt(HighScoreKey, highScore);
-                UpdateHighScoreText();
-            }
-
-            // Play the coin's collection sound
-            AudioSource coinAudioSource = other.gameObject.GetComponent<AudioSource>();
-            if (coinAudioSource != null)
-            {
-                // Play the sound
-                coinAudioSource.Play();
-
-                // Disable the collider and renderer to "hide" the coin while the sound plays
-                DisableCoinComponents(other.gameObject);
-
-                // Destroy the coin after the sound finished playing
-                Destroy(other.gameObject, coinAudioSource.clip.length);
-            }
-            else
-            {
-                // If no AudioSource found, just destroy the coin immediately
-                Destroy(other.gameObject);
-            }
-        }
-    }
-
-    private void DisableCoinComponents(GameObject coin)
-    {
-        // Disable collider and renderer to "hide" the coin
-        Collider coinCollider = coin.GetComponent<Collider>();
-        if (coinCollider != null)
-            coinCollider.enabled = false;
-
-        Renderer coinRenderer = coin.GetComponent<Renderer>();
-        if (coinRenderer != null)
-            coinRenderer.enabled = false;
-    }
-    private void UpdateHighScoreText()
-    {
-        HighScoreText.text = "High Score: " + highScore.ToString();
-    }
-}
-*/
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -88,6 +14,10 @@ public class SpawnCoinScript : MonoBehaviour
 
     private List<int> leaderboardScores = new List<int>();
     private const int MaxLeaderboardSize = 5;
+    private bool gameStartedFresh = false; // Initialization added here
+    private bool quitting = false;
+
+
 
     private void Start()
     {
@@ -95,6 +25,19 @@ public class SpawnCoinScript : MonoBehaviour
         UpdateHighScoreText();
         LoadLeaderboard();
         UpdateLeaderboardText();
+
+        // Check if the game is starting fresh or restarting
+        if (!gameStartedFresh)
+        {
+            // If it's a fresh start, reset the scoreboard
+            InitializeScoreboard();
+            gameStartedFresh = true;
+        }
+        else
+        {
+            // If it's a game restart, initialize the scoreboard without resetting
+            InitializeScoreboard();
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -148,6 +91,41 @@ public class SpawnCoinScript : MonoBehaviour
         }
 
         AddToLeaderboard(coin);
+        UpdateLeaderboardText();
+    }
+    private void OnApplicationQuit()
+    {
+        quitting = true;
+        // Reset the scoreboard and save data when quitting
+        ResetScoreboard();
+    }
+
+     public void ResetScoreboard()
+    {
+        // Only reset when quitting the game
+        if (quitting)
+        {
+            highScore = 0;
+            PlayerPrefs.SetInt(HighScoreKey, highScore);
+            UpdateHighScoreText();
+
+            leaderboardScores.Clear();
+            for (int i = 0; i < MaxLeaderboardSize; i++)
+            {
+                PlayerPrefs.SetInt("LeaderboardScore" + i, 0);
+            }
+            PlayerPrefs.Save();
+
+            coin = 0;
+            CoinText.text = "Points: " + coin.ToString();
+        }
+    }
+
+    private void InitializeScoreboard()
+    {
+        highScore = PlayerPrefs.GetInt(HighScoreKey, 0);
+        UpdateHighScoreText();
+        LoadLeaderboard();
         UpdateLeaderboardText();
     }
 
