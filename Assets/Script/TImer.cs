a1c96241c603dda4612c39f78cdbaf848efe55a0
harrytang6449 <harrytang6449@gmail.com>
2023-11-29 12:10:32 -0600

    add sountrack playing when timer has some time left

--- Assets/Script/TImer.cs
+++ Assets/Script/TImer.cs
@@ -3,16 +3,24 @@ using TMPro;
 
 public class Timer : MonoBehaviour
 {
-    public float timeRemaining = 60; // Set the timer for one minute
+    public float totalTime = 60; // Set the total time for the countdown
+    public float timeRemaining; // Current time remaining
     public TMP_Text timeText; // Reference to the TextMeshPro text component
     public GameObject gameOverPanel; // Reference to the Game Over panel
+    public AudioSource mainMusicAudioSource; // Assign the main music audio source
+    public AudioSource finalThirdMusicAudioSource; // Assign the final third audio source
     public AudioSource endSoundEffect;
+
     private bool timerIsRunning = false;
+    private bool finalThirdTriggered = false;
 
     void Start()
     {
+        timeRemaining = totalTime; // Initialize timeRemaining with totalTime
         timerIsRunning = true;
+        finalThirdTriggered = false;
         gameOverPanel.SetActive(false); // Hide the Game Over panel at start
+        mainMusicAudioSource.Play(); // Start the main music
     }
 
     void Update()
@@ -23,9 +31,25 @@ public class Timer : MonoBehaviour
             {
                 timeRemaining -= Time.deltaTime;
                 UpdateTimerDisplay(timeRemaining);
+
+                // Check if the time has reached the final third and final third music hasn't been triggered yet
+                if (!finalThirdTriggered && timeRemaining <= totalTime / 3)
+                {
+                    // Switch to the final third music
+                    mainMusicAudioSource.Stop(); // Stop the main music
+                    finalThirdMusicAudioSource.Play(); // Start the final third music
+                    finalThirdTriggered = true; // Ensure this only happens once
+                }
             }
             else
             {
+                if (!finalThirdTriggered)
+                {
+                    // If the timer reaches 0 and the final third music hasn't been triggered,
+                    // ensure that the main music stops playing.
+                    mainMusicAudioSource.Stop();
+                }
+
                 Debug.Log("Time's Up! Game Over!");
                 timeRemaining = 0;
                 timerIsRunning = false;
